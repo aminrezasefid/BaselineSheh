e691a034ae0d951a961db19001b1fd446d4f163a
armin <armin>
2024-06-30 13:13:14 +0330

    .

---
+++ torchmdnet/datasets/__init__.py
@@ -0,0 +1,10 @@
+from .qm9 import QM9
+from .md17 import MD17
+from .ani1 import ANI1
+from .custom import Custom
+from .hdf import HDF5
+from .pcqm4mv2 import PCQM4MV2_XYZ as PCQM4MV2
+from .qm7 import QM7
+from .qm8 import QM8    
+
+__all__ = ["QM9", "QM8", "QM7", "MD17", "ANI1", "Custom", "HDF5", "PCQM4MV2"]
