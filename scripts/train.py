f197cc354d949079066399159c76d57d9e160317
arminnoqteh <armin>
2024-07-03 15:29:12 +0330

    fixing wandb and testing technically

--- scripts/train.py
+++ scripts/train.py
@@ -182,11 +182,16 @@ def main():
     )
     csv_logger = CSVLogger(args.log_dir, name="", version="")
 
-    # wandb_logger = WandbLogger(name=args.job_id, project='pre-training-via-denoising', log_model = 'all', settings = wandb.Settings(start_method='fork'), notes = args.wandb_notes, save_dir = args.log_dir, id = args.job_id, tags = [args.dataset])
-
-    # ddp_plugin = None
-    # if "ddp" in args.distributed_backend:
-    #     ddp_plugin = DDPStrategy(find_unused_parameters=False)
+    wandb_logger = WandbLogger(
+        name=args.job_id,
+        project='pre-training-via-denoising',
+        log_model='all',
+        settings=wandb.Settings(start_method='thread'),  # Changed to thread to avoid forking issues
+        notes=args.wandb_notes,
+        save_dir=args.log_dir,
+        id=args.job_id + f"_{wandb.util.generate_id()}",  # Ensures unique run ID
+        tags=[args.dataset]
+    )
 
     lr_finder_callback = LearningRateFinder()
 
@@ -198,15 +203,17 @@ def main():
         default_root_dir=args.log_dir,
         # resume_from_checkpoint=args.load_model, # TODO (armin) resume_from_chechpoint is deprecated but since load_model is None at moment, we will ignore it
         callbacks=[early_stopping, checkpoint_callback],
-        logger=[tb_logger, csv_logger],
+        logger=[tb_logger, csv_logger, wandb_logger],
         reload_dataloaders_every_n_epochs= 0,
         precision=args.precision,
-        strategy= DDPStrategy(), # not supported for mps, TODO (armin) remember!
+        strategy = "ddp", # not supported for mps, REMEMBER!
     )
 
     trainer.fit(model, data)
 
-    trainer.test(dataloaders = data)
+    trainer.test(model= model, dataloaders = data)
+
+    wandb.finish()
 
 
 if __name__ == "__main__":
--- torchmdnet/datasets/qm9.py
+++ torchmdnet/datasets/qm9.py
@@ -1,45 +1,36 @@
+import os
+import os.path as osp
+import sys
+
+from typing import Callable, List, Optional, Dict
+
 import torch
+from torch import Tensor
+from torch_geometric.data import (
+    Data,
+    InMemoryDataset,
+    download_url,
+    extract_tar,
+)
 from torch_geometric.transforms import Compose
-from torch_geometric.datasets import QM9 as QM9_geometric
+from torch_geometric.utils import one_hot, scatter
+from tqdm import tqdm
 from torch_geometric.nn.models.schnet import qm9_target_dict
 
 
-class QM9(QM9_geometric):
-    def __init__(self, root, transform=None, dataset_arg=None):
-        assert dataset_arg is not None, (
-            "Please pass the desired property to "
-            'train on via "dataset_arg". Available '
-            f'properties are {", ".join(qm9_target_dict.values())}.'
-        )
+HAR2EV = 27.211386246
+KCALMOL2EV = 0.04336414
 
-        self.label = dataset_arg
-        label2idx = dict(zip(qm9_target_dict.values(), qm9_target_dict.keys()))
-        self.label_idx = label2idx[self.label]
+conversion = torch.tensor([
+    1., 1., HAR2EV, HAR2EV, HAR2EV, 1., HAR2EV, HAR2EV, HAR2EV, HAR2EV, HAR2EV,
+    1., KCALMOL2EV, KCALMOL2EV, KCALMOL2EV, KCALMOL2EV, 1., 1., 1.
+])
 
-        if transform is None:
-            transform = self._filter_label
-        else:
-            transform = Compose([transform, self._filter_label])
 
-        super(QM9, self).__init__(root, transform=transform)
+URLS = {
+    "precise3d": "https://deepchemdata.s3-us-west-1.amazonaws.com/datasets/gdb9.tar.gz"
+}
 
-    def get_atomref(self, max_z=100):
-        atomref = self.atomref(self.label_idx)
-        if atomref is None:
-            return None
-        if atomref.size(0) != max_z:
-            tmp = torch.zeros(max_z).unsqueeze(1)
-            idx = min(max_z, atomref.size(0))
-            tmp[:idx] = atomref[:idx]
-            return tmp
-        return atomref
 
-    def _filter_label(self, batch):
-        batch.y = batch.y[:, self.label_idx].unsqueeze(1)
-        return batch
-
-    def download(self):
-        super(QM9, self).download()
-
-    def process(self):
-        super(QM9, self).process()
+class QM9(InMemoryDataset):
+    pass
