b693d3f892e2cdb850392eb830e212723bdc5c7c
AlirezaSabounchi <156543423+AlirezaSabounchi@users.noreply.github.com>
2024-07-16 11:12:28 +0330

    Update __init__.py

--- torchmdnet/datasets/__init__.py
+++ torchmdnet/datasets/__init__.py
@@ -10,7 +10,7 @@ from .tox21 import TOX21
 from .esol import ESOL
 from .freesolv import FreeSolv
 from .lipophilicity import Lipophilicity
-from .sider import SIDER
+from .sider import Sider
 from .hiv import HIV
 from .toxcast import TOXCAST
 from .base import Bace       
@@ -18,4 +18,4 @@ from .bbbp import BBBP
 from .clintox import Clintox
 from .muv import MUV
 
-__all__ = ["QM9", "QM8", "QM7", "MD17", "ANI1", "Custom", "HDF5", "PCQM4MV2", "TOX21", "ESOL", "FreeSolv", "Lipophilicity", "SIDER", "HIV", "TOXCAST", "Bace", "BBBP", "Clintox", "MUV"]
+__all__ = ["QM9", "QM8", "QM7", "MD17", "ANI1", "Custom", "HDF5", "PCQM4MV2", "TOX21", "ESOL", "FreeSolv", "Lipophilicity", "Sider", "HIV", "TOXCAST", "Bace", "BBBP", "Clintox", "MUV"]
