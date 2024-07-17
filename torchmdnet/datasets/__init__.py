501eea7557f47715ef8ef5351113df8bb0656bc3
AlirezaSabounchi <156543423+AlirezaSabounchi@users.noreply.github.com>
2024-07-17 19:47:42 +0330

    Add files via upload

---
+++ torchmdnet/datasets/__init__.py
@@ -0,0 +1,21 @@
+from .qm9 import QM9
+from .md17 import MD17
+from .ani1 import ANI1
+from .custom import Custom
+from .hdf import HDF5
+from .pcqm4mv2 import PCQM4MV2_XYZ as PCQM4MV2
+from .qm7 import QM7
+from .qm8 import QM8
+from .tox21 import TOX21
+from .esol import ESOL
+from .freesolv import FreeSolv
+from .lipophilicity import Lipophilicity
+from .sider import Sider
+from .hiv import HIV
+from .toxcast import TOXCAST
+from .bace import Bace       
+from .bbbp import BBBP
+from .clintox import Clintox
+from .muv import MUV
+
+__all__ = ["QM9", "QM8", "QM7", "MD17", "ANI1", "Custom", "HDF5", "PCQM4MV2", "TOX21", "ESOL", "FreeSolv", "Lipophilicity", "Sider", "HIV", "TOXCAST", "Bace", "BBBP", "Clintox", "MUV"]
