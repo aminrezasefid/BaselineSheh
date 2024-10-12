57f2017396d10922cd2150c4be62b9f9593521ff
arminnoqteh <arminnoqteh@gmail.com>
2024-10-12 16:40:32 +0330

    just checking

--- torchmdnet/module.py
+++ torchmdnet/module.py
@@ -92,34 +92,34 @@ class LNNP(LightningModule):
         )
 
     def on_test_end(self):
-        # TODO (armin) doesn't work with multiple GPUs
-        import csv
+        if self.trainer.is_global_zero:
+            import csv
 
-        header = ["smiles"]
-        for target in self.hparams.dataset_args:
-            header.append(f"pred_{target}")
-            header.append(f"actual_{target}")
-            header.append(f"diff_{target}")
+            header = ["smiles"]
+            for target in self.hparams.dataset_args:
+                header.append(f"pred_{target}")
+                header.append(f"actual_{target}")
+                header.append(f"diff_{target}")
+                if self.hparams.task_type == "class":
+                    header.append(f"pred_{target}_class")
+            with open(self.hparams.log_dir + "/preds.csv", "w", newline="") as file:
+                writer = csv.writer(file)
+                writer.writerow(header)
+                writer.writerows(self.preds_csv)
+
+            result_dict = {
+                "test_loss": torch.stack(self.losses["test"]).mean(),
+            }
+            print(f'Test loss: {result_dict["test_loss"]}')
+
             if self.hparams.task_type == "class":
-                header.append(f"pred_{target}_class")
-        with open(self.hparams.log_dir + "/preds.csv", "w", newline="") as file:
-            writer = csv.writer(file)
-            writer.writerow(header)
-            writer.writerows(self.preds_csv)
-
-        result_dict = {
-            "test_loss": torch.stack(self.losses["test"]).mean(),
-        }
-        print(f'Test loss: {result_dict["test_loss"]}')
-
-        if self.hparams.task_type == "class":
-            result_dict["test_auc"] = torch.stack(self.auc["test"]).mean()
-            print(f'Test AUC: {result_dict["test_auc"]}')
-            with open(
-                self.hparams.log_dir + "/test_result.txt", "w", newline=""
-            ) as file:
-                file.write(str(result_dict["test_auc"].item()))
-        self.logger.log_metrics(result_dict)
+                result_dict["test_auc"] = torch.stack(self.auc["test"]).mean()
+                print(f'Test AUC: {result_dict["test_auc"]}')
+                with open(
+                    self.hparams.log_dir + "/test_result.txt", "w", newline=""
+                ) as file:
+                    file.write(str(result_dict["test_auc"].item()))
+            self.logger.log_metrics(result_dict)
 
     def step(self, batch, loss_fn, stage):
         with torch.set_grad_enabled(stage == "train" or self.hparams.derivative):
@@ -235,7 +235,7 @@ class LNNP(LightningModule):
         # if torch.isnan(loss_y):
         #     print(f"Processing data: {batch.name}")
         #     print(f"NaN loss in {batch.name}")
-        self.log("loss_y",loss_y,prog_bar=True,batch_size=batch.y.shape[0])
+        self.log("loss_y", loss_y, prog_bar=True, batch_size=batch.y.shape[0])
         return loss
 
     def optimizer_step(self, *args, **kwargs):
