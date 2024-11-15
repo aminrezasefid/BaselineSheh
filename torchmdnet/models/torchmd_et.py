1e7eff30f24d459262ad3dec8afd30a9b3b6b28e
arminnoqteh <arminnoqteh@gmail.com>
2024-11-15 08:56:45 +0330

    x mix and max

--- torchmdnet/models/torchmd_et.py
+++ torchmdnet/models/torchmd_et.py
@@ -175,7 +175,8 @@ class TorchMD_ET(nn.Module):
     def forward(self, z, pos, batch, names):
         # names = np.array(names)
         x = self.embedding(z)
-        print(x)
+        # print x's max and min
+        print(x.max(), x.min())
         edge_index, edge_weight, edge_vec = self.distance(pos, batch)
         assert (
             edge_vec is not None
